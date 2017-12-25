79e9299d94c9b7e770342fd239c5b8bb55592641
Mahendhar <mahendarreddy@live.com>
2017-12-26 00:23:10 +0530

    Max chars in a string

---
+++ maxchar/index.js
@@ -0,0 +1,42 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0, maxChar = '';
+
+  for(let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+
+    if(charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
+
+//Alternate solution
+// --------------------
+// function maxChar(str) {
+//   const charMap = {};
+//   for(let char of str) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   let max = 0, maxChar = '';
+//   for(let char in charMap) {
+//     if(charMap[char] > max) {
+//       max = charMap[char];
+//       maxChar = char;
+//     }
+//   }
+
+//   return maxChar;
+// }
+
+module.exports = maxChar;
